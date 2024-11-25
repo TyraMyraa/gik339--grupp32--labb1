e7207558a5d999ba7774c9c87a73f01e69e1d88c
VilmerSkoglund <vilmer.skoglund01@gmail.com>
2024-11-25 16:19:14 +0100

    Update Menu.js
    Hämtar checkboxen

--- Menu.js
+++ Menu.js
@@ -1,4 +1,7 @@
 const button = document.querySelector('.btn'); console.log(button);
+
+document.getElementById("divStyle")
+
 button.style.backgroundColor = "hotpink";
 
 document.body.style.backgroundColor = "pink";
