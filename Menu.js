8359cd895c54a425a8bf6c37ab23665fad65ce7e
Tyra Leu <112870397+TyraMyraa@users.noreply.github.com>
2024-11-29 14:41:46 +0100

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -1,7 +1,3 @@
 const button = document.querySelector('.btn'); console.log(button);
-
-document.getElementById("divStyle")
-
 button.style.backgroundColor = "hotpink";
-
 document.body.style.backgroundColor = "pink";
